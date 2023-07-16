e7190f1508d86b8db050e9f5a9882acc5053fe25
trsoo24 <trsoo24@naver.com>
2023-07-17 00:09:30 +0900

    File 프로젝트 생성

---
+++ TWT_BE/src/main/java/com/BE/TWT/TwtApplication.java
@@ -0,0 +1,13 @@
+package com.BE.TWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwtApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TwtApplication.class, args);
+	}
+}
