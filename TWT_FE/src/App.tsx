52a2178bcc0e4346f63efb353981ccd10272c079
eeseohyun <jinlijinyoung@gmail.com>
2023-08-03 22:43:34 +0900

    ✨ Feat(App.tsx): 라우터 추가

--- TWT_FE/src/App.tsx
+++ TWT_FE/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Selected from './pages/Selected';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/location" element={<Selected />} />
         </Routes>
       </BrowserRouter>
     </div>
