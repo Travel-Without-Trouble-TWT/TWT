5e5e9346c31cc3ae6ab6212df8ad734a73031c25
eeseohyun <jinlijinyoung@gmail.com>
2023-08-09 21:34:33 +0900

    🍎 Chore: tailwindCSS 커스텀 유틸리티 추가

--- TWT_FE/tailwind.config.js
+++ TWT_FE/tailwind.config.js
@@ -2,18 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
-    colors: {
-      white: '#ffffff',
-      skyblue: '#90DCE1',
-      blue: '#379EFD',
-      gray: '#9B9B9B',
-      red: '#F40D0D',
-      darkgray: '#1f2937',
-      lightgray: '#F6F6F6',
-      active: 'rgba(0, 0, 0, 0.8)',
-      hoverLink: 'rgba(0, 0, 0, 0.6)',
-      unActive: 'rgba(0, 0, 0, 0.3)',
+    extend: {
+      colors: {
+        white: '#ffffff',
+        yellow: '#FFCC02',
+        skyblue: '#90DCE1',
+        blue: '#379EFD',
+        gray: '#9B9B9B',
+        red: '#F40D0D',
+        darkgray: '#1f2937',
+        lightgray: '#F6F6F6',
+        active: 'rgba(0, 0, 0, 0.8)',
+        hoverLink: 'rgba(0, 0, 0, 0.6)',
+        unActive: 'rgba(0, 0, 0, 0.3)',
+      },
     },
     screens: {
       mobile: '360px', // @media (min-width: 360px)
@@ -24,5 +26,17 @@ module.exports = {
       '2xl': '1460px', // @media (min-width: 1460px)
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.touch-action-pan-y': {
+          'touch-action': 'pan-y',
+        },
+        'will-change-transform': {
+          'will-change': 'transform',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
 };
