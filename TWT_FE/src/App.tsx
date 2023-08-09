c1e833504e32c59016520674006111a8861c909f
eeseohyun <jinlijinyoung@gmail.com>
2023-08-09 21:35:13 +0900

    ✨ Feat(App.tsx): 상세페이지 라우터 추가

--- TWT_FE/src/App.tsx
+++ TWT_FE/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/detail" element={<Detail />} />
         </Routes>
       </BrowserRouter>
     </div>
