fa9557c36261859f5bab476d31e15f978f5af8c1
eeseohyun <jinlijinyoung@gmail.com>
2023-08-15 22:43:31 +0900

    ✨ Feat(App.tsx): 일정상세 라우터 추가

--- TWT_FE/src/App.tsx
+++ TWT_FE/src/App.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Schedule from './pages/Schedule';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route></Route>
+          <Route path="/schedule" element={<Schedule />} />
         </Routes>
       </BrowserRouter>
     </div>
