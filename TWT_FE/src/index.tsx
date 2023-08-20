006bbf55d1cb0ed24ab94180db5b4805b3d22398
eeseohyun <jinlijinyoung@gmail.com>
2023-08-20 22:34:54 +0900

    🍎 Chore: Context API 추가

--- TWT_FE/src/index.tsx
+++ TWT_FE/src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App';
+import { StateContextProvider } from './context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +13,9 @@ let queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
