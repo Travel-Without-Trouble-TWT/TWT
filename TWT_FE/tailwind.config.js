8c2232e6f4c526ca728d6847706ea068efb3f30a
eeseohyun <jinlijinyoung@gmail.com>
2023-08-23 01:45:45 +0900

    🍎 Chore: tailwindcss.config 수정

--- TWT_FE/tailwind.config.js
+++ TWT_FE/tailwind.config.js
@@ -2,19 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
-    colors: {
-      white: '#ffffff',
-      skyblue: '#90DCE1',
-      blue: '#379EFD',
-      gray: '#9B9B9B',
-      red: '#F40D0D',
-      darkgray: '#1f2937',
-      lightgray: '#F6F6F6',
-      active: 'rgba(0, 0, 0, 0.8)',
-      hoverLink: 'rgba(0, 0, 0, 0.6)',
-      unActive: 'rgba(0, 0, 0, 0.3)',
+    extend: {
+      colors: {
+        white: '#ffffff',
+        skyblue: '#90DCE1',
+        blue: '#379EFD',
+        gray: '#9B9B9B',
+        red: '#F40D0D',
+        darkgray: '#1f2937',
+        lightgray: '#F6F6F6',
+        active: 'rgba(0, 0, 0, 0.8)',
+        hoverLink: 'rgba(0, 0, 0, 0.6)',
+        unActive: 'rgba(0, 0, 0, 0.3)',
+      },
     },
+
     screens: {
       mobile: '360px', // @media (min-width: 360px)
       foldable: '523px', // @media (min-width: 523px)
