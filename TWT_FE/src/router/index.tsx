6bf4cbca4addeac2054a8afcf390e8299afb3496
eeseohyun <jinlijinyoung@gmail.com>
2023-08-23 01:51:19 +0900

    ✨ Feat(src/router/index.tsx): join router 추가

--- TWT_FE/src/router/index.tsx
+++ TWT_FE/src/router/index.tsx
@@ -3,6 +3,7 @@ import type { RouteObject } from 'react-router-dom';
 
 //컴포넌트 호출
 import Login from '../pages/Login';
+import Join from '../pages/Join';
 
 const authRoutes: RouteObject = {
   path: '*',
@@ -11,5 +12,12 @@ const authRoutes: RouteObject = {
       path: 'login',
       element: <Login />,
     },
+    {
+      path: 'join',
+      element: <Join />,
+    },
   ],
 };
+
+const routes: RouteObject[] = [authRoutes];
+export default routes;
