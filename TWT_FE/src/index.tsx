dcb60a24ca68d893726d1d4dacf3133cb8af3195
eeseohyun <jinlijinyoung@gmail.com>
2023-08-28 21:26:51 +0900

    ✨ Feat(index.tsx): ReactQueryDevtools 추가

--- TWT_FE/src/index.tsx
+++ TWT_FE/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import App from './App';
 
@@ -13,6 +14,7 @@ root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
