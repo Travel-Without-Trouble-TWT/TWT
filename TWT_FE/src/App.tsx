bbedaccea438c2014f6b8c5d197ef0ae5d1c4d10
eeseohyun <jinlijinyoung@gmail.com>
2023-08-30 17:15:14 +0900

    📁 File(Login, Join): 파일 제거

--- TWT_FE/src/App.tsx
+++ TWT_FE/src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import Login from './pages/Login';
-import Join from './pages/Join';
 
 function App() {
   return (
@@ -9,8 +7,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
         </Routes>
       </BrowserRouter>
     </div>
