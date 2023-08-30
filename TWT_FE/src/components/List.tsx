e8885281feab57e635e226a1c85756311a5550e4
eeseohyun <jinlijinyoung@gmail.com>
2023-08-30 16:59:18 +0900

    ✨ Feat(List.tsx): api 데이터 연결

--- TWT_FE/src/components/List.tsx
+++ TWT_FE/src/components/List.tsx
@@ -1,4 +1,14 @@
-function List() {
+import { Top10Props } from '../api/type';
+
+function List({
+  id,
+  placeName,
+  placeType,
+  placeLocation,
+  star,
+  placeHeart,
+  placeImageUrl,
+}: Top10Props) {
   return (
     <section className="dark:bg-gray-900">
       <div className="container px-6 mx-auto">
@@ -6,25 +16,25 @@ function List() {
           <div className="lg:flex">
             <img
               className="object-cover w-full h-56 rounded-lg lg:w-64"
-              src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-              alt=""
+              src={placeImageUrl}
+              alt={placeName}
             />
 
             <div className="flex flex-col justify-between py-4 lg:mx-6">
               <a
-                href="/"
+                href={`/${id}`}
                 className="text-xl font-semibold hover:underline dark:text-white"
               >
-                Title
+                {placeName}
               </a>
               <div className="flex items-center">
                 <span className="text-yellow-300">★</span>
                 <p className="ml-1 text-sm font-semibold text-gray dark:text-white">
-                  4.0
+                  {star}
                 </p>
                 <span className="w-1 h-1 mx-0.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
                 <a
-                  href="/"
+                  href={`/${id}`}
                   className="text-sm font-medium text-gray hover:underline dark:text-white"
                 >
                   0개의 리뷰
@@ -33,10 +43,12 @@ function List() {
               <div className="flex items-center">
                 <p className="text-red">♥︎</p>
                 <p className="ml-1 text-sm font-semibold text-gray dark:text-white">
-                  +99
+                  {placeHeart}
                 </p>
               </div>
-              <span className="text-sm dark:text-gray-300">description</span>
+              <span className="text-sm dark:text-gray-300">
+                {placeType} | {placeLocation}
+              </span>
             </div>
           </div>
         </div>
