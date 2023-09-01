246d2b9f881944d7d2ddb3e7d5cbe1fbdb778632
eeseohyun <jinlijinyoung@gmail.com>
2023-09-02 04:53:42 +0900

    ✨ Feat(Mypage 페이지): 해당 지역 일정리스트 불러오기 기능 및 카테고리 분류

--- TWT_FE/src/pages/Mypage.tsx
+++ TWT_FE/src/pages/Mypage.tsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import { TfiWrite } from 'react-icons/tfi';
 import { AiTwotoneCalendar } from 'react-icons/ai';
 
 import SouthKoreaMap from '../components/SouthKoreaMap';
+import ScheduleList from '../components/ScheduleList';
+import { useUserDatas } from '../hooks/useProducts';
 
 function Mypage() {
+  const [isListOpen, setIsListOpen] = useState<string | null>(null);
+  const [category, setCategory] = useState<string>('schedule');
+  const {
+    userDatas,
+    fetchNextPage,
+    hasNextPage,
+    userDataLoading,
+    userDataError,
+  } = useUserDatas(category);
   return (
     <>
       <section className="bg-lightgray dark:bg-gray-900 h-full flex justify-center flex-col lg:px-20 px-10 py-6">
@@ -21,18 +33,33 @@ function Mypage() {
           </div>
           <div className="flex-1 flex flex-col items-center justify-end px-8 mt-2">
             <div className="flex items-center space-x-4 mt-2">
-              <button className="flex items-center bg-blue hover:opacity-90 text-white hover:text-black px-4 py-2 rounded-xl text-sm space-x-2 transition duration-100">
+              <button
+                onClick={() => setCategory('schedule')}
+                className={`flex items-center bg-skyblue/80 hover:bg-skyblue text-white hover:text-black px-4 py-2 rounded-xl text-sm space-x-2 transition duration-100 ${
+                  category === 'schedule' && 'bg-skyblue text-black'
+                }`}
+              >
                 <span className="flex items-center gap-2">
                   <AiTwotoneCalendar />
                   일정
                 </span>
               </button>
-              <button className="flex items-center bg-blue hover:opacity-90 text-white hover:text-black px-4 py-2 rounded-xl text-sm space-x-2 transition duration-100">
+              <button
+                onClick={() => setCategory('review')}
+                className={`flex items-center bg-skyblue/80 hover:bg-skyblue text-white hover:text-black px-4 py-2 rounded-xl text-sm space-x-2 transition duration-100 ${
+                  category === 'review' && 'bg-skyblue text-black'
+                }`}
+              >
                 <span className="flex items-center gap-2">
                   <TfiWrite /> 리뷰
                 </span>
               </button>
-              <button className="flex items-center bg-blue hover:opacity-90 text-white hover:text-black px-4 py-2 rounded-xl text-sm space-x-2 transition duration-100">
+              <button
+                onClick={() => setCategory('heart')}
+                className={`flex items-center bg-skyblue/80 hover:bg-skyblue text-white hover:text-black px-4 py-2 rounded-xl text-sm space-x-2 transition duration-100 ${
+                  category === 'heart' && 'bg-skyblue text-black'
+                }`}
+              >
                 <span>♡ 좋아요</span>
               </button>
             </div>
@@ -40,7 +67,15 @@ function Mypage() {
         </div>
         <div className="bg-white rounded-lg shadow-xl pb-8">
           <div className="flex justify-center p-10">
-            <SouthKoreaMap />
+            <SouthKoreaMap
+              isListOpen={isListOpen}
+              setIsListOpen={setIsListOpen}
+            />
+            {isListOpen && !userDataLoading && (
+              <div>
+                <ScheduleList userDatas={userDatas} isListOpen={isListOpen} />
+              </div>
+            )}
           </div>
         </div>
       </section>
