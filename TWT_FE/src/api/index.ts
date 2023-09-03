cffe81a9b815a5068301c447ee1d72d186b0d9d8
eeseohyun <jinlijinyoung@gmail.com>
2023-09-04 02:39:26 +0900

    ✨ Feat(src/api/index.ts): multipart/form-data api 기능 구현

--- TWT_FE/src/api/index.ts
+++ TWT_FE/src/api/index.ts
@@ -1,32 +1,77 @@
 import axios from 'axios';
-import { Top10Props, ReviewProps } from './type';
+import { authApi } from './auth';
+import { Top10Props, ReviewProps, ScheduleProps } from './type';
 
+//방문지 top10
 export const getTop10Fn = async () => {
   const response = await axios.get<Top10Props>('/search/main'); //authAPI로 변경예정
   return response.data;
 };
 
-export const getReviewsFn = async (page: number) => {
-  const response = await axios.get<ReviewProps>(
+//스케쥴들
+export const getSchedulesFn = async (page: number) => {
+  const response = await authApi.get<ScheduleProps>(
     `/search/recent/pageNum=${page + 1}`
   );
   return response.data;
 };
 
+//선택페이지 리스트들
 export const getPlaceFn = async (
   placeLocation: string,
   placeType: string,
   page: number
 ) => {
-  const response = await axios.get<Top10Props>(
+  const response = await authApi.get<Top10Props>(
     `/search/location?placeLocation=${placeLocation}&placeType=${placeType}&pageNum=${page}`
   );
   return response.data;
 };
 
+//마이페이지
 export const getUserDataFn = async (category: string, page: number) => {
-  const response = await axios.get(
+  const response = await authApi.get(
     `/search/member/${category}?pageNum=${page + 1}`
   );
   return response.data;
 };
+
+//리뷰 리스트들
+export const getPlaceReviewsFn = async (page: number) => {
+  const response = await authApi.get<ReviewProps>(
+    `/review/place?placeId=&pageNum=${page}`
+  );
+  return response.data;
+};
+
+//해당 장소 정보
+export const getPlaceInfoFn = async (placeId: number) => {
+  const response = await authApi.get(`/search/detail?placeId=${placeId}`);
+  return response.data;
+};
+
+//post
+export const postReviewFn = async (data, files) => {
+  const formData = new FormData();
+
+  const requestObj = {
+    reviewComment: data.reviewComment,
+    placeId: data.placeId,
+    star: data.star,
+  };
+
+  const requestBlob = new Blob([JSON.stringify(requestObj)], {
+    type: 'application/json',
+  });
+
+  formData.append('request', requestBlob);
+  formData.append('files', files);
+
+  const response = authApi.post('/review/place', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  return response;
+};
+//
