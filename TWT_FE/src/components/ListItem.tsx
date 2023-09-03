a117cfdbaa7d62c3fba723cfd7cc218a01ddcf23
eeseohyun <jinlijinyoung@gmail.com>
2023-09-04 08:08:34 +0900

    ⭐️ Style(ListItem 컴포넌트): 다크모드 적용

--- TWT_FE/src/components/ListItem.tsx
+++ TWT_FE/src/components/ListItem.tsx
@@ -11,7 +11,7 @@ function ListItem({
   reviewNum,
 }: Top10Props) {
   return (
-    <div className="lg:flex">
+    <div className="lg:flex dark:bg-slate-900">
       <img
         className="object-cover w-full h-56 rounded-lg lg:w-64"
         src={placeImageUrl}
@@ -26,25 +26,25 @@ function ListItem({
           {placeName}
         </a>
         <div className="flex items-center">
-          <span className="text-yellow-300">★</span>
+          <span className="text-yellow-300 text-yellow">★</span>
           <p className="ml-1 text-sm font-semibold text-gray dark:text-white">
             {star}
           </p>
           <span className="w-1 h-1 mx-0.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
           <a
             href={`/${id}`}
-            className="text-sm font-medium text-gray hover:underline dark:text-white"
+            className="text-sm font-medium text-gray hover:underline dark:text-gray-300"
           >
-            {reviewNum}개의 리뷰
+            {reviewNum === 0 ? '0' : reviewNum}개의 리뷰
           </a>
         </div>
         <div className="flex items-center">
           <p className="text-red">♥︎</p>
-          <p className="ml-1 text-sm font-semibold text-gray dark:text-white">
+          <p className="ml-1 text-sm font-semibold text-slate-500 dark:text-white">
             {placeHeart}
           </p>
         </div>
-        <span className="text-sm dark:text-gray-300">
+        <span className="text-sm text-slate-500 dark:text-gray-300">
           {placeType} | {placeLocation}
         </span>
       </div>
