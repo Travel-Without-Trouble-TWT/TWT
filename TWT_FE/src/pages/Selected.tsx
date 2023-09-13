2ffdabbd61158492e1abc6aac8cf7d058b9d5d06
eeseohyun <jinlijinyoung@gmail.com>
2023-09-13 23:07:41 +0900

    🧠 Refactor(Selected 페이지): 코드 리팩토링(똑같은 기능인데 코드만 개선)

--- TWT_FE/src/pages/Selected.tsx
+++ TWT_FE/src/pages/Selected.tsx
@@ -1,44 +1,50 @@
 import { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Map, CustomOverlayMap } from 'react-kakao-maps-sdk';
+import { useQueryClient } from '@tanstack/react-query';
 //component
-import InfiniteScroll from 'react-infinite-scroll-component';
 import Spinner from '../components/Spinner';
+import PlaceLists from '../components/PlaceLists';
 //icons
 import { MdOutlineFoodBank, MdOutlineAttractions } from 'react-icons/md';
 import { AiOutlineHome } from 'react-icons/ai';
 import { usePlaces } from '../hooks/useProducts';
+import { DataProps } from '../api/type';
+import { getPlaceFn } from '../api';
+
+const buttonData = [
+  { id: 'ALL', text: '전체' },
+  { id: 'HOT_PLACE', text: '명소', icon: <MdOutlineAttractions /> },
+  { id: 'RESTAURANT', text: '맛집', icon: <MdOutlineFoodBank /> },
+  { id: 'STAY', text: '숙소', icon: <AiOutlineHome /> },
+];
 
 function Selected() {
   const [placeType, setPlaceType] = useState<string>('ALL'); //"ALL", "STAY", "RESTAURANT", "HOT_PLACE"
   const [isTitleOpen, setIsTitleOpen] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [selectedPlace, setSelectedPlace] = useState({ lat: null, lng: null });
 
-  const { location, type } = useParams();
+  const queryClient = useQueryClient();
+  const { location } = useParams();
   const placeLocation = decodeURIComponent(String(location));
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    //현재 url에서 placeType 파라미터 추출
-    if (type) {
-      setPlaceType(type);
-    }
-  }, [location, type]);
-
-  const {
-    places,
-    fetchNextPage,
-    hasNextPage,
-    placeLoading,
-    placeError,
-    center,
-  } = usePlaces(placeType, placeLocation);
+  const { places, placeLoading, placeError, center } = usePlaces(
+    placeType,
+    placeLocation,
+    currentPage - 1
+  );
 
   const handleLocationButtonClick = (newPlaceType: string) => {
-    // 버튼 클릭 시 URL 업데이트
     setPlaceType(newPlaceType);
-    fetchNextPage({ pageParam: 0 });
-    navigate(`/search/${placeLocation}/${newPlaceType}`);
+    setCurrentPage(1);
   };
+
+  useEffect(() => {
+    queryClient.fetchQuery(['places'], () =>
+      getPlaceFn(placeType, placeLocation, currentPage - 1)
+    );
+  }, [placeType, currentPage, queryClient]);
 
   return (
     <>
@@ -49,88 +55,30 @@ function Selected() {
           </h1>
           <div className="py-3 flex justify-between border-b border-lightgray mb-4">
             <div className="flex">
-              <button
-                className="flex text-sm border-2 px-2 py-1 border-lightgray rounded-md mr-2 hover:bg-lightgray transition-colors duration-300 items-center"
-                id="ALL"
-                onClick={() => handleLocationButtonClick('ALL')}
-              >
-                전체
-              </button>
-              <button
-                className="flex text-sm border-2 px-2 py-1 border-lightgray rounded-md mr-2 hover:bg-lightgray transition-colors duration-300 items-center"
-                id="HOT_PLACE"
-                onClick={() => handleLocationButtonClick('HOT_PLACE')}
-              >
-                <MdOutlineAttractions />
-                명소
-              </button>
-              <button
-                className="flex text-sm border-2 px-2 py-1 border-lightgray rounded-md mr-2 hover:bg-lightgray transition-colors duration-300 items-center"
-                id="RESTAURANT"
-                onClick={() => handleLocationButtonClick('RESTAURANT')}
-              >
-                <MdOutlineFoodBank />
-                맛집
-              </button>
-              <button
-                className="flex text-sm border-2 px-2 py-1 border-lightgray rounded-md mr-2 hover:bg-lightgray transition-colors duration-300 items-center"
-                id="STAY"
-                onClick={() => handleLocationButtonClick('STAY')}
-              >
-                <AiOutlineHome />
-                숙소
-              </button>
+              {buttonData.map((button) => (
+                <button
+                  key={button.id}
+                  className="flex text-sm border-2 px-2 py-1 border-lightgray rounded-md mr-2 hover:bg-lightgray hover:font-semibold transition-colors duration-300 items-center focus:bg-lightgray focus:font-semibold"
+                  id={button.id}
+                  onClick={() => handleLocationButtonClick(button.id)}
+                >
+                  {button.icon}
+                  {button.text}
+                </button>
+              ))}
             </div>
           </div>
           <div className="grid lg:grid-cols-2 lg:gap-1 flex-col lg:flex-row w-full">
-            <div>
-              {placeLoading ? (
-                <Spinner />
-              ) : (
-                <>
-                  {places ? (
-                    places?.pages.map((content) => (
-                      <div key={content.id}>
-                        <div className="h-32 flex justify-between border-b-2 border-lightgray">
-                          <div className="flex p-3">
-                            <img
-                              className="w-[130px] h-full mr-4"
-                              src={content.placeImageUrl}
-                            />
-                            <div className="flex flex-col justify-evenly">
-                              <p
-                                className="text-lg font-bold leading-5 hover:underline hover:cursor-pointer"
-                                role="button"
-                                onClick={() => setIsTitleOpen(content.id)}
-                              >
-                                {content.placeName}
-                              </p>
-                              <p className="leading-5 text-sm">
-                                {content.placeAddress}
-                              </p>
-                              <p className="text-gray leading-5 text-sm">
-                                {content.placeType} |{content.placeLocation}
-                              </p>
-                            </div>
-                          </div>
-                          <div className="flex self-center mr-3">
-                            <button className="text-blue text-sm bg-skyblue bg-opacity-20 px-3 py-1 rounded-2xl">
-                              선택
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    ))
-                  ) : (
-                    <span>데이터가 존재하지 않습니다.</span>
-                  )}
-                </>
-              )}
-            </div>
-
+            <PlaceLists
+              places={places}
+              placeLoading={placeLoading}
+              setIsTitleOpen={setIsTitleOpen}
+              setCurrentPage={setCurrentPage}
+              currentPage={currentPage}
+            />
             <div className="grid flex-grow lg:w-full lg:h-full h-[590px] z-0">
               {placeLoading ? (
-                <Spinner />
+                <Spinner size={'30px'} />
               ) : (
                 <>
                   <Map
@@ -139,7 +87,7 @@ function Selected() {
                     level={10}
                   >
                     {places ? (
-                      places?.pages.map((content) => (
+                      places.content.map((content: DataProps) => (
                         <CustomOverlayMap
                           position={{
                             lat: content.latitude,
@@ -148,11 +96,22 @@ function Selected() {
                           key={content.id}
                         >
                           <div
-                            className={`z-10 flex items-center justify-center text-white -translate-x-1/2 rounded-full bg-amber-500 ring-2 ring-white ${
-                              isTitleOpen && 'py-1 px-3 gap-1'
+                            className={`flex items-center justify-center text-white -translate-x-1/2 rounded-full bg-amber-500 ring-2 ring-white ${
+                              isTitleOpen === content.id &&
+                              'py-1 px-3 gap-1 z-10'
+                            } ${
+                              content.placeType === 'STAY' && 'bg-pink-500'
+                            } ${
+                              content.placeType === 'RESTAURANT' && 'bg-blue'
                             }`}
                           >
-                            <MdOutlineAttractions />
+                            {content.placeType === 'HOT_PLACE' && (
+                              <MdOutlineAttractions />
+                            )}
+                            {content.placeType === 'RESTAURANT' && (
+                              <MdOutlineFoodBank />
+                            )}
+                            {content.placeType === 'STAY' && <AiOutlineHome />}
                             {isTitleOpen === content.id && (
                               <div className="flex flex-col">
                                 <span className="text-sm font-semibold">
@@ -173,7 +132,9 @@ function Selected() {
                         </CustomOverlayMap>
                       ))
                     ) : (
-                      <span>데이터가 존재하지 않습니다.</span>
+                      <span className="flex justify-center items-center text-gray font-semibold">
+                        데이터가 존재하지 않습니다.
+                      </span>
                     )}
                   </Map>
                 </>
