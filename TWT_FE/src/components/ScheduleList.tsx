152707f5751b6b4228913d1290a08b4f91e81f0f
eeseohyun <jinlijinyoung@gmail.com>
2023-09-17 22:12:35 +0900

    ⭐️ Style(ScheduleList 컴포넌트): data 배치

--- TWT_FE/src/components/ScheduleList.tsx
+++ TWT_FE/src/components/ScheduleList.tsx
@@ -8,21 +8,24 @@ function ScheduleList({
   return (
     <>
       <ul className="divide-y divide-slate-100 shadow-md ml-5 rounded">
-        <li className="flex items-center gap-4 px-4 py-3">
-          <div className="flex items-center self-center">
-            <img
-              src="https://tailwindmix.b-cdn.net/products/product-shoe-01.jpeg"
-              alt="schedule image"
-              className="w-8 rounded"
-            />
-          </div>
+        {userDatas &&
+          userDatas.content.map((userData) => {
+            <li key={userData.id} className="flex items-center gap-4 px-4 py-3">
+              <div className="flex items-center self-center">
+                <img
+                  src={userData.scheduleImageUrl}
+                  alt="schedule image"
+                  className="w-8 rounded"
+                />
+              </div>
 
-          <div className="flex min-h-[2rem] min-w-0 flex-1 flex-col items-start justify-center gap-0">
-            <span className="w-full truncate text-sm text-gray">
-              2023.07.07-2023.07.10
-            </span>
-          </div>
-        </li>
+              <div className="flex min-h-[2rem] min-w-0 flex-1 flex-col items-start justify-center gap-0">
+                <span className="w-full truncate text-sm text-gray">
+                  {userData.scheduleName} {userData.startAt} ~ {userData.endAt}
+                </span>
+              </div>
+            </li>;
+          })()}
       </ul>
     </>
   );
