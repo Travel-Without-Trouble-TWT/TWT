855c4891852dae290cda3c271bcdf02dbfbc62e6
eeseohyun <jinlijinyoung@gmail.com>
2023-09-18 21:54:05 +0900

    ✨ Feat(ReviewList 컴포넌트): ReviewList 불러오기 기능 추가 및 목업데이터 삭제

--- TWT_FE/src/components/ReviewList.tsx
+++ TWT_FE/src/components/ReviewList.tsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Stars from './Stars';
+import Pagination from './Pagination';
 import ZoomModal from './ZoomModal';
 
-function ReviewList(data) {
+import { useReviews } from '../hooks/useProducts';
+import Spinner from './Spinner';
+
+function ReviewList({ placeId }: { placeId: number }) {
   const [selectedImages, setSelectedImages] = useState<string[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  const { reviews, reviewLoading, reviewError, reviewRefetch } = useReviews(
+    placeId,
+    currentPage - 1
+  );
 
   // 이미지 클릭 이벤트 핸들러
   const handleImageClick = (images: string[], index: number) => {
@@ -14,46 +24,30 @@ function ReviewList(data) {
     setIsModalOpen(true);
   };
 
-  const reviews = [
-    {
-      createAt: '2023-08-01',
-      id: 0,
-      nickName: '닉네임',
-      reviewComment: '여기 내가 가본 곳 중에 원탑임.',
-      reviewImageList: [
-        'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MTZfNzQg/MDAxNjYwNjE4Nzg0MzI0.nnU6bkwd9pyfnfZ_f1n0guaFPljXrXGPDK1ndDDT7b4g.eyufddGemHi5Ht8M5cQpZAweaaKd7ih_Kp-U-KHjG-8g.JPEG.hkwoozi/20220816%EF%BC%BF115912.jpg?type=w800',
-        'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MTZfMjg1/MDAxNjYwNjE4Nzg0ODY3.XW7zJ6GMN5Jd8Ja_KVq2Bw-fKI3rqnGVQHjxq5OSIkIg.xTD0rTk1EdxS7No0MEykgPUjJ9HgOKpW4pQjjTivfZEg.JPEG.hkwoozi/20220816%EF%BC%BF115913.jpg?type=w800',
-      ],
-      star: 4.5,
-    },
-    {
-      createAt: '2023-08-20',
-      id: 1,
-      nickName: '닉네임',
-      reviewComment: '그냥 그럼',
-      reviewImageList: [
-        'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MTZfNzQg/MDAxNjYwNjE4Nzg0MzI0.nnU6bkwd9pyfnfZ_f1n0guaFPljXrXGPDK1ndDDT7b4g.eyufddGemHi5Ht8M5cQpZAweaaKd7ih_Kp-U-KHjG-8g.JPEG.hkwoozi/20220816%EF%BC%BF115912.jpg?type=w800',
-        'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MTZfMjg1/MDAxNjYwNjE4Nzg0ODY3.XW7zJ6GMN5Jd8Ja_KVq2Bw-fKI3rqnGVQHjxq5OSIkIg.xTD0rTk1EdxS7No0MEykgPUjJ9HgOKpW4pQjjTivfZEg.JPEG.hkwoozi/20220816%EF%BC%BF115913.jpg?type=w800',
-      ],
-      star: 2.5,
-    },
-    {
-      createAt: '2023-08-21',
-      id: 2,
-      nickName: '닉네임',
-      reviewComment: '여기 내가 가본 곳 중에 원탑임.',
-      reviewImageList: [
-        'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MTZfNzQg/MDAxNjYwNjE4Nzg0MzI0.nnU6bkwd9pyfnfZ_f1n0guaFPljXrXGPDK1ndDDT7b4g.eyufddGemHi5Ht8M5cQpZAweaaKd7ih_Kp-U-KHjG-8g.JPEG.hkwoozi/20220816%EF%BC%BF115912.jpg?type=w800',
-        'https://mblogthumb-phinf.pstatic.net/MjAyMjA4MTZfMjg1/MDAxNjYwNjE4Nzg0ODY3.XW7zJ6GMN5Jd8Ja_KVq2Bw-fKI3rqnGVQHjxq5OSIkIg.xTD0rTk1EdxS7No0MEykgPUjJ9HgOKpW4pQjjTivfZEg.JPEG.hkwoozi/20220816%EF%BC%BF115913.jpg?type=w800',
-      ],
-      star: 5,
-    },
-  ];
+  const handlePageChange = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  useEffect(() => {
+    reviewRefetch();
+  }, [currentPage]);
+
   return (
     <>
-      {reviews &&
-        reviews.map((review, index) => (
-          <div className="flex w-full flex-col divide-y border-b border-slate-300 py-2">
+      {!reviews ||
+        (reviews.content.length === 0 && (
+          <span className="font-semibold text-gray p-10">
+            장소에 대한 리뷰가 아직 없습니다.
+          </span>
+        ))}
+      {!reviews && reviewLoading ? (
+        <Spinner size={'20px'} />
+      ) : (
+        reviews.content.map((review) => (
+          <div
+            key={review.id}
+            className="flex w-full flex-col divide-y border-b border-slate-300 py-2"
+          >
             <div className="flex flex-col gap-2 py-4">
               <h4 className="flex w-full flex-1 gap-4 text-base font-medium text-slate-700">
                 <div className="w-0 flex-1 flex gap-2 items-center">
@@ -65,8 +59,15 @@ function ReviewList(data) {
                     <span className="truncate text-sm font-semibold text-slate-700 dark:text-white">
                       {review.nickName}
                     </span>
-                    <Stars size={'w-4 h-4'} rating={review.star} />
-                    <span className="text-xs text-gray">{review.createAt}</span>
+                    <Stars
+                      size={'w-4 h-4'}
+                      rating={review.star}
+                      key={review.id}
+                    />
+                    <span className="text-xs text-gray">
+                      {review.createAt.split(':')[0]}:
+                      {review.createAt.split(':')[1]}
+                    </span>
                   </div>
                 </div>
               </h4>
@@ -88,8 +89,13 @@ function ReviewList(data) {
               </p>
             </div>
           </div>
-        ))}
-
+        ))
+      )}
+      <Pagination
+        currentPage={currentPage}
+        totalPages={reviews ? reviews.totalPages : 0}
+        onPageChange={handlePageChange}
+      />
       {isModalOpen && (
         <ZoomModal
           setShowModal={() => setIsModalOpen(false)}
