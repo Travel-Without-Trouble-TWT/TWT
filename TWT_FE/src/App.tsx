0cfc4572e5c776ba15e34cf8e89e34c4114a68c4
eeseohyun <jinlijinyoung@gmail.com>
2023-09-19 20:08:44 +0900

    ✨ Feat(Profile 페이지): 프로필 수정 페이지 추가

--- TWT_FE/src/App.tsx
+++ TWT_FE/src/App.tsx
@@ -8,6 +8,7 @@ import Selected from './pages/Selected';
 import Mypage from './pages/Mypage';
 import Schedule from './pages/Schedule';
 import Detail from './pages/Detail';
+import Profile from './pages/Profile';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -24,6 +25,7 @@ function App() {
           <Route path="/join" element={<Join />} />
           <Route path="/search/:location" element={<Selected />} />
           <Route path="/mypage" element={<Mypage />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
         <Footer />
       </BrowserRouter>
