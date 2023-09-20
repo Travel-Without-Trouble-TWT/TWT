6c926d9e8c43fbdeb4ac1d64236d77f2f0b00816
eeseohyun <jinlijinyoung@gmail.com>
2023-09-20 23:59:15 +0900

    🧠 Refactor(src/context): 코드 리팩토링(똑같은 기능인데 코드만 개선)

--- TWT_FE/src/context/index.tsx
+++ TWT_FE/src/context/index.tsx
@@ -1,11 +1,4 @@
-import React, {
-  createContext,
-  useContext,
-  useState,
-  ReactNode,
-  useMemo,
-  useEffect,
-} from 'react';
+import React, { createContext, useContext, ReactNode, useMemo } from 'react';
 import { useUserInfo } from '../hooks/useAuth';
 
 type User = {
@@ -14,21 +7,16 @@ type User = {
   profileUrl: string;
 };
 
-const accessToken = localStorage.getItem('accessToken');
 export const UserContext = createContext<{
   isLogin: boolean;
   user: User | null;
 }>({
-  isLogin: accessToken !== null ? true : false,
+  isLogin: false,
   user: null,
 });
 
 export function useUserContext() {
-  const context = useContext(UserContext);
-  if (!context) {
-    throw new Error('useUserContext는 UserProvider 내에서 사용해야 합니다');
-  }
-  return context;
+  return useContext(UserContext);
 }
 
 type UserProviderProps = {
@@ -36,23 +24,12 @@ type UserProviderProps = {
 };
 
 export function UserProvider({ children }: UserProviderProps) {
-  const [isLogin, setIsLogin] = useState<boolean>(
-    accessToken !== null ? true : false
-  );
-  const [user, setUser] = useState<User | null>(null);
-
-  const value = useMemo(
-    () => ({ isLogin, setIsLogin, user }),
-    [isLogin, setIsLogin, user]
-  );
   const { userInfo, userInfoLoading, userInfoError } = useUserInfo();
-  useEffect(() => {
-    if (isLogin) {
-      if (!userInfoLoading && !userInfoError) {
-        setUser(userInfo);
-      }
-    }
-  }, [isLogin, userInfo, userInfoLoading, userInfoError]);
+  const contextValue = useMemo(() => {
+    return { isLogin: !!userInfo, user: userInfo || null };
+  }, [userInfo]);
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
 }
