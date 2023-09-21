f1d9d7d42738429c4564fc365c2cd5e31a429afa
eeseohyun <jinlijinyoung@gmail.com>
2023-09-22 04:47:14 +0900

    ✨ Feat(Schedule 페이지): 삭제 기능 추가

--- TWT_FE/src/pages/Schedule.tsx
+++ TWT_FE/src/pages/Schedule.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useUserContext } from '../context';
+import { useNavigate, useParams } from 'react-router-dom';
 
 //components
 import TimeModal from '../components/TimeModal';
@@ -10,19 +11,56 @@ import DateModal from '../components/DateModal';
 import Spinner from '../components/Spinner';
 import CustomMap from '../components/CustomMap';
 //hooks
-import { useSchedule } from '../hooks/useProducts';
+import {
+  useDeletePlace,
+  useDeleteSchedule,
+  useSchedule,
+} from '../hooks/useProducts';
 //icons
 import { FaShareSquare } from 'react-icons/fa';
 
 function Schedule() {
   const [isMoreOpen, setIsMoreOpen] = useState<string | null>(null);
   const [isShowTimeModal, setIsShowTimeModal] = useState<boolean>(false);
-  const [isShowAlert, setIsShowAlert] = useState<string>('');
+  const [isShowAlert, setIsShowAlert] = useState<string | number>('');
   const [isShowDateModal, setIsShowDateModal] = useState<boolean>(false);
   const { id } = useParams();
   const scheduleId = Number(id);
+  const { isLogin } = useUserContext();
+  const navigate = useNavigate();
 
   const { schedule, scheduleLoading, scheduleError } = useSchedule(scheduleId);
+  const [deleteData, setDeleteData] = useState({
+    dayScheduleId: null,
+    index: null,
+  });
+  const {
+    deleteSchedule,
+    scheduleDeleting,
+    scheduleDeletingSuccess,
+    scheduleDeletingError,
+  } = useDeleteSchedule(scheduleId);
+  const { deletePlace, placeDeleting, placeDeleteSuccess, placeDeleteError } =
+    useDeletePlace(deleteData);
+
+  if (scheduleDeletingSuccess)
+    return (
+      <Alerts
+        type="success"
+        title="삭제 성공"
+        message="일정이 삭제되었습니다."
+        onConfirm={() => navigate('/mypage')}
+      />
+    );
+  if (placeDeleteSuccess) {
+    return (
+      <Alerts
+        type="success"
+        title="삭제 성공"
+        message="해당 장소가 삭제되었습니다."
+      />
+    );
+  }
 
   return (
     <>
@@ -46,12 +84,14 @@ function Schedule() {
                 <span className="text-white underline">
                   {schedule.startAt}~{schedule.endAt}
                 </span>
-                <button
-                  className="text-gray text-sm"
-                  onClick={() => setIsShowDateModal(true)}
-                >
-                  편집
-                </button>
+                {isLogin && (
+                  <button
+                    className="text-gray text-sm"
+                    onClick={() => setIsShowDateModal(true)}
+                  >
+                    편집
+                  </button>
+                )}
               </div>
               <span className="font-bold text-2xl text-white">
                 D
@@ -69,12 +109,14 @@ function Schedule() {
               <button className="flex self-end mr-3 text-white -mt-6 z-20 text-xl">
                 <FaShareSquare />
               </button>
-              <button
-                onClick={() => setIsShowAlert('일정삭제')}
-                className="flex self-end mr-4 mt-2 text-gray text-sm z-20"
-              >
-                일정 삭제
-              </button>
+              {isLogin && (
+                <button
+                  onClick={() => setIsShowAlert('일정삭제')}
+                  className="flex self-end mr-4 mt-2 text-gray text-sm z-20"
+                >
+                  일정 삭제
+                </button>
+              )}
               <img
                 alt="일정 이미지"
                 src={schedule?.scheduleImageUrl}
@@ -84,11 +126,11 @@ function Schedule() {
 
             <div className="px-6 m-auto mt-10">
               {/* Day별 */}
-              {schedule.dayScheduleList.length > 0 ? (
-                schedule.dayScheduleList.map((item) => (
+              {schedule.dayScheduleList.length > 0 &&
+                schedule.dayScheduleList.map((item, idx) => (
                   <div className="grid grid-cols-4 gap-3 tablet:grid-cols-8 lg:grid-cols-12">
                     <div className="flex flex-col items-center justify-center col-span-1 lg:col-span-1">
-                      <p className="text-xl font-semibold">Day</p>
+                      <p className="text-xl font-semibold">Day{idx + 1}</p>
                       <p className="text-xs text-gray">{item.day}</p>
                     </div>
                     <div className="col-span-7 lg:col-span-11 shadow-md shadow-gray rounded-md px-5 py-5">
@@ -97,29 +139,34 @@ function Schedule() {
                           <Feeds
                             setIsShowTimeModal={setIsShowTimeModal}
                             setIsShowAlert={setIsShowAlert}
-                            data={item.courseList}
+                            dayScheduleId={schedule.dayScheduleList.id}
+                            setDeleteData={setDeleteData}
+                            data={
+                              isMoreOpen === item.id
+                                ? item.courseList
+                                : item.courseList.slice(0, 3)
+                            }
                             placeLocation={schedule.travelPlace}
                           />
-                          {isMoreOpen === item.id && item.courseList && (
-                            <CustomMap data={item} />
+                          {isMoreOpen === item.id &&
+                            item.courseList.length > 0 && (
+                              <CustomMap data={item} />
+                            )}
+                          {item.courseList.length > 0 && (
+                            <div className="flex justify-center mb-1 mt-2">
+                              <button
+                                className="text-sm"
+                                onClick={() => setIsMoreOpen(item.id)}
+                              >
+                                {isMoreOpen === item.id ? '닫기' : '더보기'}
+                              </button>
+                            </div>
                           )}
                         </>
                       )}
-
-                      <div className="flex justify-center mb-1 mt-2">
-                        <button
-                          className="text-sm"
-                          onClick={() => setIsMoreOpen(item.id)}
-                        >
-                          {isMoreOpen === item.id ? '닫기' : '더보기'}
-                        </button>
-                      </div>
                     </div>
                   </div>
-                ))
-              ) : (
-                <span>스케쥴을 추가해주세요.</span>
-              )}
+                ))}
             </div>
           </div>
         </section>
@@ -129,14 +176,16 @@ function Schedule() {
         <Alerts
           title="일정 삭제"
           message="정말로 일정을 삭제하시겠습니까?"
-          type="warn"
+          type="error"
+          onConfirm={() => deleteSchedule(scheduleId)}
         />
       )}
       {isShowAlert === '장소삭제' && (
         <Alerts
           title="장소 삭제"
           message="정말로 장소를 삭제하시겠습니까?"
-          type="warn"
+          type="error"
+          onConfirm={() => deletePlace(deleteData)}
         />
       )}
       {isShowTimeModal && <TimeModal setIsShowModal={setIsShowTimeModal} />}
