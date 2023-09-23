1f805b6faa7a5d75acf7fea79ccbad12e4b910e4
eeseohyun <jinlijinyoung@gmail.com>
2023-09-24 01:21:39 +0900

    🎨 Design(ScheduleList 컴포넌트): 다크모드 추가

--- TWT_FE/src/components/ScheduleList.tsx
+++ TWT_FE/src/components/ScheduleList.tsx
@@ -7,7 +7,7 @@ function ScheduleList({
 }) {
   return (
     <>
-      <ul className="divide-y divide-slate-100 shadow-md ml-5 rounded">
+      <ul className="divide-y divide-slate-100 dark:divide-slate-800 shadow-md ml-5 rounded dark:bg-slate-700">
         {userDatas ? (
           userDatas.content.map((userData: any) => (
             <li key={userData.id} className="flex items-center gap-4 px-4 py-3">
@@ -20,7 +20,7 @@ function ScheduleList({
               </div>
 
               <div className="flex min-h-[2rem] min-w-0 flex-1 flex-col items-start justify-center gap-0">
-                <span className="w-full truncate text-sm hover:font-semibold">
+                <span className="w-full truncate text-sm dark:text-gray dark:hover:text-white">
                   <a href={`/schedule/${userData.id}`}>
                     {userData.scheduleName}({userData.startAt}~{userData.endAt})
                   </a>
