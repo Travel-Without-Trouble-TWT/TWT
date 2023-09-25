904a0e71b34369ea2565090a43dd48dd2a7caa14
eeseohyun <jinlijinyoung@gmail.com>
2023-09-26 02:27:32 +0900

    ✨ Feat(src/hooks/useAuth): 로그아웃, 닉네임 및 프로필 수정, 알람 hook 추가

--- TWT_FE/src/hooks/useAuth.ts
+++ TWT_FE/src/hooks/useAuth.ts
@@ -1,11 +1,17 @@
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { LoginProps } from '../api/type';
-import { loginFn } from '../api/auth';
+import {
+  editNickNameFn,
+  editProfileImgFn,
+  getEmitterFn,
+  loginFn,
+  logoutFn,
+} from '../api/auth';
 import { getUserInfoFn } from '../api';
 import { subscribeFn } from '../api/auth';
 
-//로그인 로직
+//로그인
 export const useLogin = () => {
   const { mutate: loginUser, isLoading: logining } = useMutation(
     (userData: LoginProps) => loginFn(userData),
@@ -20,6 +26,44 @@ export const useLogin = () => {
   return { loginUser, logining };
 };
 
+//로그아웃
+export const useLogout = () => {
+  const {
+    mutate: logout,
+    isLoading: logouting,
+    isError: logoutError,
+  } = useMutation(() => logoutFn(), {
+    onSuccess: () => {
+      localStorage.removeItem('accessToken');
+    },
+  });
+  return { logout, logouting, logoutError };
+};
+
+export const useEditNickName = (nickname: string) => {
+  const {
+    mutate: editNickName,
+    isLoading: nickNameEditing,
+    isError: nickNameError,
+  } = useMutation(() => editNickNameFn(nickname), {});
+  return { editNickName, nickNameEditing, nickNameError };
+};
+
+export const useEditProfileImg = (file: File) => {
+  const queryClient = useQueryClient();
+  const {
+    mutate: editProfileImg,
+    isLoading: editingProfileImg,
+    isError: editingProfileImgError,
+  } = useMutation(() => editProfileImgFn(file), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['userInfo']);
+    },
+    onError: (error) => console.log('업로드 중 오류 발생'),
+  });
+  return { editProfileImg, editingProfileImg, editingProfileImgError };
+};
+
 //유저 정보
 export const useUserInfo = () => {
   const {
@@ -30,4 +74,14 @@ export const useUserInfo = () => {
     staleTime: 6 * 60 * 60 * 1000,
   });
   return { userInfo, userInfoLoading, userInfoError };
+};
+
+//알람 정보
+export const useGetEmitters = () => {
+  const {
+    data: emitters,
+    isLoading: emittersGetting,
+    isError: emittersError,
+  } = useQuery(['emitters'], () => getEmitterFn(), {});
+  return { emitters, emittersGetting, emittersError };
 };
