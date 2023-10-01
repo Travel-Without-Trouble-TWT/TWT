d5a0a9a5b3e82c5cbc89a897943de99c5bd6fdcd
eeseohyun <jinlijinyoung@gmail.com>
2023-10-01 18:08:22 +0900

    ⭐️ Style(Spinner 컴포넌트): 코드 포맷팅, 코드 오타, 함수명 수정 등 스타일 수정

--- TWT_FE/src/components/Spinner.tsx
+++ TWT_FE/src/components/Spinner.tsx
@@ -1,30 +1,30 @@
-function Spinner({ size }: any) {
+export default function Spinner({ size }: { size: string }) {
   return (
-    <div className="flex w-full min-h-full justify-center items-center">
+    <>
       <svg
-        width={size}
-        height={size}
         viewBox="0 0 24 24"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         aria-live="polite"
         aria-busy="true"
+        aria-labelledby="title-01b desc-01b"
+        className={`w-${size} h-${size} animate animate-spin`}
       >
-        <path
-          d="M7 10H3V14H7V10Z"
-          className="animate animate-bounce fill-skyblue "
+        <title id="title-01b">Icon title</title>
+        <desc id="desc-01b">Some desc</desc>
+        <circle
+          cx="12"
+          cy="12"
+          r="10"
+          className="stroke-slate-200"
+          strokeWidth="4"
         />
         <path
-          d="M14 10H10V14H14V10Z"
-          className="animate animate-bounce fill-skyblue [animation-delay:.2s]"
-        />
-        <path
-          d="M21 10H17V14H21V10Z"
-          className="animate animate-bounce fill-skyblue [animation-delay:.4s]"
+          d="M22 12C22 10.6868 21.7413 9.38642 21.2388 8.17317C20.7362 6.95991 19.9997 5.85752 19.0711 4.92893C18.1425 4.00035 17.0401 3.26375 15.8268 2.7612C14.6136 2.25866 13.3132 2 12 2"
+          className="stroke-skyblue"
+          strokeWidth="4"
         />
       </svg>
-    </div>
+    </>
   );
 }
-
-export default Spinner;
