3a9e1f33d1bb356a7a86bf5c779d3badb1237afc
eeseohyun <jinlijinyoung@gmail.com>
2023-10-01 18:07:57 +0900

    ⭐️ Style(Loader 컴포넌트): 코드 포맷팅, 코드 오타, 함수명 수정 등 스타일 수정

--- TWT_FE/src/components/Loader.tsx
+++ TWT_FE/src/components/Loader.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
-
-function Loader() {
+function Loader({ size }: any) {
   return (
-    <div className="flex gap-1">
-      <div className="w-2 h-2 rounded-full animate-pulse bg-skyblue"></div>
-      <div className="w-2 h-2 rounded-full animate-pulse bg-skyblue"></div>
-      <div className="w-2 h-2 rounded-full animate-pulse bg-skyblue"></div>
+    <div className="flex w-full min-h-full justify-center items-center">
+      <svg
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        aria-live="polite"
+        aria-busy="true"
+      >
+        <path
+          d="M7 10H3V14H7V10Z"
+          className="animate animate-bounce fill-skyblue "
+        />
+        <path
+          d="M14 10H10V14H14V10Z"
+          className="animate animate-bounce fill-skyblue [animation-delay:.2s]"
+        />
+        <path
+          d="M21 10H17V14H21V10Z"
+          className="animate animate-bounce fill-skyblue [animation-delay:.4s]"
+        />
+      </svg>
     </div>
   );
 }
