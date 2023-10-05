c5ddf520edd9b7a614715aa808a6e0d011028ea4
eeseohyun <jinlijinyoung@gmail.com>
2023-10-05 19:57:51 +0900

    🎨 Design(ReviewCard 컴포넌트): 반응형 디자인

--- TWT_FE/src/components/ReviewCard.tsx
+++ TWT_FE/src/components/ReviewCard.tsx
@@ -12,7 +12,7 @@ function ReviewCard({ data }: any) {
         perView: 2,
         autoplay: 3000,
         animationDuration: 1000,
-        gap: 24,
+        gap: 10,
         breakpoints: {
           1024: {
             perView: 2,
@@ -34,14 +34,14 @@ function ReviewCard({ data }: any) {
   }, [data]);
 
   return (
-    <div className="glide-01 w-full relative">
+    <div className="glide-01 w-full relative mt-10">
       <div className="overflow-hidden" data-glide-el="track">
         <div className="flex whitespace-no-wrap flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] touch-pan-y will-change-transform w-full h-full overflow-hidden p-2 shadow">
           {data &&
             data.length > 0 &&
             data.map((item) => (
               <div
-                className="relative flex flex-col rounded-xl w-[400px] mx-auto p-4 bg-white bg-clip-border shadow dark:bg-gray-800 dark:text-white dark:!shadow-none"
+                className="relative flex flex-col rounded-xl w-[400px] mx-auto p-4 bg-white bg-clip-border shadow dark:bg-slate-800 dark:text-white dark:!shadow-none"
                 key={item.id}
               >
                 <div className="relative flex h-32 w-full rounded-xl bg-cover">
@@ -50,7 +50,7 @@ function ReviewCard({ data }: any) {
                     alt="스케쥴 대표 이미지"
                     className="absolute flex h-32 w-full justify-center rounded-xl bg-cover"
                   />
-                  <div className="absolute -bottom-12 flex ml-5 h-[80px] w-[80px] rounded-full border-[4px] border-white dark:!border-gray-800">
+                  <div className="absolute -bottom-12 flex ml-5 h-[80px] w-[80px] rounded-full border-[4px] border-white dark:!border-slate-800">
                     <img
                       className="h-full w-full rounded-full"
                       src={
@@ -63,7 +63,7 @@ function ReviewCard({ data }: any) {
                   </div>
                 </div>
                 <div className="mt-14 ml-2">
-                  <span className="text-sm text-gray dark:text-white">
+                  <span className="text-sm text-gray dark:text-slate-400 xs:text-xs">
                     {item.member.nickName}님의 일정 | {item.startAt} ~{' '}
                     {item.endAt}
                   </span>
