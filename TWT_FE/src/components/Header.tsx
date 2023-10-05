4d9ec2ccb48d1e35220eb63ed25b401490dda714
eeseohyun <jinlijinyoung@gmail.com>
2023-10-05 20:04:34 +0900

    🎨 Design(Header 컴포넌트): 스타일 수정

--- TWT_FE/src/components/Header.tsx
+++ TWT_FE/src/components/Header.tsx
@@ -16,8 +16,8 @@ function Header() {
   const { emitters, emittersGetting, emittersError } = useGetEmitters();
 
   return (
-    <header className="border-b-1 relative sticky z-50 w-full border-b border-slate-200 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full  lg:border-slate-200 lg:backdrop-blur-sm lg:after:hidden to-sky-50 bg-gradient-to-b from-[#A5E0F8]">
-      <div className="relative mx-auto max-w-full px-6 lg:max-w-5xl xl:max-w-7xl 2xl:max-w-[96rem]">
+    <header className="border-b-1 relative sticky z-50 w-full border-b border-slate-200 shadow-lg shadow-slate-700/5 after:absolute after:top-full after:left-0 after:z-10 after:block after:h-px after:w-full  xl:border-slate-200 xl:backdrop-blur-sm xl:after:hidden to-sky-50 bg-gradient-to-b from-[#A5E0F8]">
+      <div className="relative mx-auto max-w-full px-6 md:max-w-5xl lg:max-w-7xl xl:max-w-[96rem]">
         <nav
           aria-label="main navigation"
           className="flex h-[5.5rem] items-stretch justify-between font-medium text-slate-700"
@@ -27,15 +27,15 @@ function Header() {
             id="TWT"
             aria-label="TWT logo"
             aria-current="page"
-            className="flex items-center whitespace-nowrap py-3 text-lg focus:outline-none lg:flex-1"
+            className="flex items-center whitespace-nowrap py-3 text-lg focus:outline-none xl:flex-1"
             href="/"
           >
             <img
-              className="h-[90px] w-[90px] self-center py-3"
+              className="h-[90px] w-[90px] self-center py-3 xs:h-[80px] xs:w-[80px]"
               src={logo}
               alt="로고 이미지"
             />
-            <span className="flex flex-col text-xs ml-[-19px] mb-3">
+            <span className="flex flex-col text-xs ml-[-19px] mb-3 xs:text-[0.2rem]">
               <span className="text-slate-500">
                 <strong className="text-black">T</strong>ravel
               </span>
@@ -48,7 +48,7 @@ function Header() {
             </span>
           </a>
           <button
-            className={`relative order-10 block h-10 w-10 self-center lg:hidden
+            className={`relative order-10 block h-10 w-10 self-center xl:hidden lg:hidden
           ${
             isToggleOpen
               ? 'visible opacity-100 [&_span:nth-child(1)]:w-6 [&_span:nth-child(1)]:translate-y-0 [&_span:nth-child(1)]:rotate-45 [&_span:nth-child(3)]:w-0 [&_span:nth-child(2)]:-rotate-45 '
@@ -77,7 +77,7 @@ function Header() {
           <ul
             role="menubar"
             aria-label="Select page"
-            className={`absolute top-0 left-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 lg:visible lg:relative lg:top-0  lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ${
+            className={`absolute top-0 left-0 z-[-1] h-[28.5rem] w-full justify-center overflow-hidden  overflow-y-auto overscroll-contain bg-white/90 px-8 pb-12 pt-24 font-medium transition-[opacity,visibility] duration-300 xl:visible xl:relative xl:top-0 xl:z-0 xl:flex xl:h-full xl:w-auto xl:items-stretch xl:overflow-visible xl:bg-white/0 xl:px-0 xl:py-0 xl:pt-0 xl:opacity-100 lg:visible lg:relative lg:top-0  lg:z-0 lg:flex lg:h-full lg:w-auto lg:items-stretch lg:overflow-visible lg:bg-white/0 lg:px-0 lg:py-0  lg:pt-0 lg:opacity-100 ${
               isToggleOpen
                 ? 'visible opacity-100 backdrop-blur-sm'
                 : 'invisible opacity-0'
@@ -90,7 +90,7 @@ function Header() {
                     role="menuitem"
                     aria-haspopup="false"
                     tabIndex={0}
-                    className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-blue focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8"
+                    className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-blue focus:bg-emerald-50 focus:outline-none focus-visible:outline-none lg:px-8 xl:px-8"
                     href="/mypage"
                   >
                     <span>마이페이지</span>
@@ -102,7 +102,7 @@ function Header() {
                     aria-current="page"
                     aria-haspopup="false"
                     tabIndex={0}
-                    className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-blue lg:px-8"
+                    className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-blue lg:px-8 xl:px-8"
                     href="/profile"
                   >
                     <span>프로필 수정</span>
@@ -114,7 +114,7 @@ function Header() {
                     role="menuitem"
                     aria-haspopup="false"
                     tabIndex={0}
-                    className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-blue lg:px-8"
+                    className="flex items-center gap-2 py-4 transition-colors duration-300 hover:text-blue lg:px-8 xl:px-8"
                   >
                     <span>로그아웃</span>
                   </span>
@@ -122,7 +122,7 @@ function Header() {
               </>
             ) : null}
           </ul>
-          <div className="ml-auto flex items-center px-6 lg:ml-0 lg:p-0">
+          <div className="ml-auto flex items-center px-6 lg:ml-0 lg:p-0 xl:ml-0 xl:p-0">
             {isLogin ? (
               <>
                 <div className="relative inline-flex h-10 w-10 items-center justify-center rounded-full text-white">
