d6eaa651305afb6a1beeb7fddf4b1a4463f62f98
eeseohyun <jinlijinyoung@gmail.com>
2023-10-27 05:03:38 +0900

    🧠 Refactor(Main 페이지): 코드 리팩토링(똑같은 기능인데 코드만 개선)

--- TWT_FE/src/pages/Main.tsx
+++ TWT_FE/src/pages/Main.tsx
@@ -3,6 +3,8 @@ import Searchbar from '../components/Searchbar';
 import ReviewCard from '../components/ReviewCard';
 import Loader from '../components/Loader';
 import ListItem from '../components/ListItem';
+import Error from '../components/Error';
+
 import { useSchedules, useTop10 } from '../hooks/useProducts';
 import { useState } from 'react';
 
@@ -13,16 +15,7 @@ function Main() {
   const { top10, top10Loading, top10Error } = useTop10();
 
   if (schedulesError || top10Error) {
-    return (
-      <div className="min-w-full min-h-screen flex justify-center items-center py-3 dark:bg-slate-950">
-        <div className="w-2/3 flex flex-col py-1 justify-center items-center sm:w-full xs:w-full">
-          <p className="text-red-500 font-bold text-xl">
-            ⚠️ 데이터를 불러오는 중 오류가 발생했습니다.
-          </p>
-          <p className="text-gray-500">나중에 다시 시도해주세요.</p>
-        </div>
-      </div>
-    );
+    return <Error />;
   }
 
   return (
